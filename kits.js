6ead916fa4985c81d4388bd67d4c550e4a76ea65
钟昆 <1064598959@qq.com>
2019-11-09 18:27:42 +0800

    第一次上次

---
+++ kits.js
@@ -0,0 +1,34 @@
+// 封装一个kits 对象
+var kits = {};
+// 随机整数
+kits.randomInt = function(n, m) {
+  return Math.floor(Math.random() * (m - n + 1) + n);
+};
+//rgb随机颜色
+kits.randomColor = function() {
+  var r = kits.randomInt(0, 255);
+  var g = kits.randomInt(0, 255);
+  var b = kits.randomInt(0, 255);
+  return (color = "rgb(" + r + "," + g + "," + b + ")");
+};
+// 随机十六进制颜色
+kits.randomHexColor = function () {
+    var arr = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    var color = ['#'];
+    for (var i = 0; i < 6; i++) {
+        var r = kits.randomInt(0, 16);
+        color.push(arr[r]);
+    }
+    return color.join('');
+}
+//随机十六进制颜色
+kits.randomHex1Color = function () {
+    var color = ['#'];
+    for (var i = 0; i < 6; i++) {
+        var r = kits.randomInt(0, 16).toString(16);
+        color.push(r);
+        
+      
+    }
+    return color.join('');
+}
